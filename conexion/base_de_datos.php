46d530d7d9e4cd76dd2da830e799e4c4e0354e3e
maxidesantafe87 <52078179+maxidesantafe87@users.noreply.github.com>
2023-03-09 11:49:16 -0300

    Update base_de_datos.php

--- conexion/base_de_datos.php
+++ conexion/base_de_datos.php
@@ -1,8 +1,8 @@
 <?php
 
 $contraseña = "";
-$usuario = "root";
-$nombre_base_de_datos = "codoacodo";
+$usuario = "id20019168_codoacodo";
+$nombre_base_de_datos = "id20019168_codoacodo";
 try{
 	$base_de_datos = new PDO('mysql:host=localhost;dbname=' . $nombre_base_de_datos, $usuario, $contraseña);
 }catch(Exception $e){
